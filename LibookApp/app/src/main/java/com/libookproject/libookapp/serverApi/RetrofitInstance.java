6a7299cac4d263571431fcfaf9e5b95de603ddb6
adi <adikoren1912@gmail.com>
2026-06-21 00:41:18 +0300

    Server@App - setup database

--- LibookApp/app/src/main/java/com/libookproject/libookapp/serverApi/RetrofitInstance.java
+++ LibookApp/app/src/main/java/com/libookproject/libookapp/serverApi/RetrofitInstance.java
@@ -10,7 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class RetrofitInstance
 {
-    private static final String BASE_URL = "http://10.0.2.2:8000/";
+    private static final String BASE_URL = "https://libook-api.onrender.com";
     private static Retrofit retrofit;
 
     /**
